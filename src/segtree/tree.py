009fe088aa3c8c4615c0f4ecd90bba5d4192b811
Sam Peters <sam@sl27.net>
2022-02-28 14:10:33 -0800

    Fixed Segtree build method breaking when an empty list is passed to it

--- src/segtree/tree.py
+++ src/segtree/tree.py
@@ -7,10 +7,16 @@ class SegmentTree:
         n = len(array)
         self.lb = 0
         self.rb = n-1
-        self._tree: List[Optional[int]] = [0 for i in range(4 * n)]
-        self._build(array, 1, self.lb, self.rb)
-        
-    def sum(self, l: int, r: int, ti: int = 1, lb: int = self.lb, rb: int = self.rb) -> int:
+        self._tree: List[int] = [0 for i in range(4 * n + 2)]
+        if self.rb >= 0:
+            self._build(array, 1, self.lb, self.rb)
+
+
+    def sum(self, l: int, r: int, ti: int = 1, lb: Optional[int] = None, rb: Optional[int] = None) -> int:
+        if lb is None:
+            lb = self.lb
+        if rb is None:
+            rb = self.rb
         if l == lb and r == rb:
             return self._tree[ti]
         elif  l <= lb and r >= rb:
@@ -20,11 +26,16 @@ class SegmentTree:
             midpoint = self._midpoint(lb, rb)
             if self._overlap(l, r, lb, midpoint):
                 left = self.sum(l, r, self._lchild(ti), lb=lb, rb=midpoint)
-            if self._overlap(l, r, self._rchild(ti))
+            if self._overlap(l, r, midpoint+1, rb):
+                right = self.sum(l, r, self._rchild(ti), lb=midpoint+1, rb=rb)
+            return left + right
 
 
-    def _overlap(l1: int, r1: int, l2: int, r2: int):
-        return l1 <= l2 and r1 <= r2 or l2 <= l1 and r2 <= r1
+    def _overlap(self, l1: int, r1: int, l2: int, r2: int):
+        if r2 > r1:
+            return l2 <= max(l1, r1)
+        else:
+            return l1 <= max(l2, r2)
 
     def _build(self, a: List[int], ti: int, lb: int, rb: int):
         segment_sum = 0
@@ -38,7 +49,7 @@ class SegmentTree:
             # right child
             self._build(a, self._rchild(ti), midpoint + 1, rb)
 
-    def _midpoint(lb: int, rb: int) -> int:
+    def _midpoint(self, lb: int, rb: int) -> int:
         return (lb + rb) // 2
 
     def _parent(self, i: int) -> int:
@@ -51,3 +62,6 @@ class SegmentTree:
 
     def _rchild(self, i: int) -> int:
         return 2 * i + 1
+
+t = SegmentTree([2])
+t.sum(0,2)
